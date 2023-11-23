4ff6e01d8201b62c6f092c5ac9e20112f3f50b52
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2023-11-23 19:31:12 +0700

    Update addSSL.py

--- addSSL.py
+++ addSSL.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 # ... Cấu hình ứng dụng Flask của bạn ở đây ...
 
 if __name__ == '__main__':
-    context = ('/home/ubuntu/LoanPredictAPI/api.hutech.click/fullchain.pem', '/home/ubuntu/LoanPredictAPI/api.hutech.click/privkey.pem')
+    context = ('/home/ubuntu/LoanPredictAPI/fullchain.pem', '/home/ubuntu/LoanPredictAPI/privkey.pem')
     app.run(ssl_context=context)
